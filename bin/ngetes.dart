ad4c1e1a0a77c9045d094d2d763b5e31a0fa4d47
AndikaBN <bintangnursalih275@gmail.com>
2023-03-01 15:30:55 +0800

    commit awal

---
+++ bin/ngetes.dart
@@ -0,0 +1,5 @@
+import 'package:ngetes/ngetes.dart' as ngetes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ngetes.calculate()}!');
+}
---
+++ test/ngetes_test.dart
@@ -0,0 +1,8 @@
+import 'package:ngetes/ngetes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
